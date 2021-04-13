b24922d745225815c5bc4238c7bc21253cadfdf6
flaviodiniz <flvdiniz91@gmail.com>
2021-04-12 22:44:04 -0300

    adição do projeto

---
+++ ifpbtv/src/main/java/com/tv/ifpb/IfpbtvApplication.java
@@ -0,0 +1,13 @@
+package com.tv.ifpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfpbtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfpbtvApplication.class, args);
+	}
+
+}
---
+++ ifpbtv/src/test/java/com/tv/ifpb/IfpbtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tv.ifpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfpbtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
